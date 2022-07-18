c671952b813bcdce353c5db0053290bf203dd1d1
404 <fabrucemaminiaina@gmail.com>
2022-07-18 12:16:11 +0300

    Mise en place environnement Backend

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.listen(5000,async () =>{
+    console.log("connected")
+    // await sequelize.authenticate()
+    // console.log("database connected")
+})
