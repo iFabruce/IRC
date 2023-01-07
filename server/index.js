9e05dfe9849df63e996c83ed9e50199c7456b780
Maminiaina Fabruce <fabrucemaminiaina@gmail.com>
2023-01-07 15:29:08 +0300

    UPDATE INDEX

--- server/index.js
+++ server/index.js
@@ -1,21 +1,8 @@
-//Express
 const express = require('express')
 const app = express()
-//Cookies
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
-
 app.use(express.json())
-
-// var cors = require('cors')
-// //Cors
-// const corsConfig = {
-//     origin: '*',
-//     credentials: false,
-// };  
-// app.use(cors());
-// app.options('*', cors(corsConfig));
-
 const cors = require('cors');
 const origin = process.env.NODE_ENV === "development" 
   ? "http://localhost:3000" 
@@ -27,34 +14,11 @@ app.use(
     origin
   }),
 );
-
-//Routes
 const route = require('./routes/routes')
 app.use(route) 
 
-const {sequelize, Utilisateur, Compte, Abonnement} = require('./models');    //Models
-
-app.get('/utilisateurs', async (req,res) =>{
-    try {
-        const users = await Utilisateur.findAll({include: 'comptes'})
-        
-        return res.json(users)
-    } catch (error) {
-        return res.json(error)
-    }
-})
-
-app.get('/comptes', async (req,res) =>{
-    try {
-        const comptes = await Compte.findAll()
-        return res.json(comptes)
-    } catch (error) {
-        return res.json(error)
-    }
-})
-
-
-app.listen(9002,async () =>{
+const PORT = process.env.PORT || 5000
+app.listen(PORT,async () =>{
     console.log("loading...")
     await sequelize.authenticate()
     console.log("sequelize!")
